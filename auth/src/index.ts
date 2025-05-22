b097704d30cd4c0edd3926b8d403310c29c209be
MarkBastoulis <mbastoulis@gmail.com>
2025-05-22 23:11:37 +1000

    error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -6,6 +7,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -14,6 +16,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// If a route is used that is not any of the prior ones, it is not found
+app.all('*', async () => {
+  // Express will capture this error and throw it to error-handling middleware
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
