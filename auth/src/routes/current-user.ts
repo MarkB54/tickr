120fde4a521a5bd6f2c15499af1d1791ffd23b3a
MarkBastoulis <mbastoulis@gmail.com>
2025-05-23 21:40:42 +1000

    Refactored currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  // If req.session does not exist (evaluate to true) or
-  // req.session.jwt does not exist
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // if the token has been tampered with, verify() will throw an error
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    // Send the payload
-    res.send({ currentUser: payload });
-  } catch (err) {
-    // Say they are not logged in
-    return res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
