37e34b5e40b39aa44f042824a88f1e08c39dbf58
MarkBastoulis <mbastoulis@gmail.com>
2025-05-23 11:22:16 +1000

    Creating a user functionality

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -21,10 +21,23 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    // console.log('Creating a user...');
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    const { email, password } = req.body;
+
+    // Query over the user collection by using the UserModel
+    // If existingUser is null, we have not found an existing user with that email
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
+
+    // Create the user and save to mongoDB
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
