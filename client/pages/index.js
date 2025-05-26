a25b9baf2c1228ba04c36c55f1427b8b264afc54
MarkBastoulis <mbastoulis@gmail.com>
2025-05-26 16:13:44 +1000

    Loading page signin vs not signin

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,22 @@
-const Landing = () => {
-  return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-export default Landing;
+// this is not a component, it is a function
+// it is executed on the server
+LandingPage.getInitialProps = async (context) => {
+  // buildClient gets us an axios instance
+  const client = buildClient(context);
+  // We still need to make a request using it (.get(...) in this case)
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
