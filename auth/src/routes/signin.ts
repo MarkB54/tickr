c959c1654ee59e66882a3126eef7df60400cde8d
MarkBastoulis <mbastoulis@gmail.com>
2025-05-27 12:16:46 +1000

    small changes to get it working with the common module

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@mbtickr/common';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-requests';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
