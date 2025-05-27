fae2a65b0bc29040dbf222475b7efdfbfb65a4cf
MarkBastoulis <mbastoulis@gmail.com>
2025-05-27 12:23:34 +1000

    Set up for ticketing service

---
+++ tickets/src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@mbtickr/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // Do not encrypt the seesion information
+    signed: false,
+    // Require user to be on a HTTPS connection
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+// If a route is used that is not any of the prior ones, it is not found
+app.all('*', async () => {
+  // Express will capture this error and throw it to error-handling middleware
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
